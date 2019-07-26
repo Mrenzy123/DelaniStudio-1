2cf3be85da0e5114b2748628676682f926bc765c
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-07-26 15:09:13 +0200

    add jQuery to fourth section. Add bold styling to h5.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
 $(document). ready(function(){
-    
-  
+    $("#design").click(function(){
+        $("img#designimage").toggle();
+        $("p#designimage").toggle();
+        $("h5#design1").toggle();
+        $("p#design1").toggle();
+    });
+    $("#development").click(function(){
+        $("img#designdevt").toggle();
+        $("p#designdevt").toggle();
+        $("h5#design2").toggle();
+        $("p#design2").toggle();
+    });
+    $("#product").click(function(){
+        $("img#designprod").toggle();
+        $("p#designprod").toggle();
+        $("h5#design3").toggle();
+        $("p#design3").toggle();
+    }); 
 });
